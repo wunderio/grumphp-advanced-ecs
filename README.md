d0c18601f0ee771ede12f3e96aab19d257949593
Guntis <guntis.jakovins@wunder.io>
2019-09-10 12:48:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# grumphp-advanced-ecs
+
+Easy coding standards implementation to better react on task context.
+
+### grumphp.yml:
+````yml
+parameters:
+    tasks:
+        ecs:
+          config: ecs.yml
+          whitelist_patterns: ['web/modules/custom', 'web/themes/custom']
+          triggered_by: [php, inc, module, install]
+          clear-cache: false
+          no-progress-bar: true
+    extensions:
+        - Wunderio\ECS\Task\ExtensionLoader
+````
+
+### Composer
+
+``composer require --dev wunderio/grumphp-advanced-ecs``
---
+++ src/Task/ExtensionLoader.php
@@ -0,0 +1,32 @@
+<?php
+namespace Wunderio\ECS\Task;
+
+use GrumPHP\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Class ExtensionLoader
+ * @package wunderio\PhpCheckSyntaxTask
+ */
+class ExtensionLoader implements ExtensionInterface
+{
+  /**
+   * @param ContainerBuilder $container
+   *
+   * @return \Symfony\Component\DependencyInjection\Definition
+   * @throws \Exception
+   * @throws \Symfony\Component\DependencyInjection\Exception\BadMethodCallException
+   * @throws \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException
+   * @throws \Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException
+   * @throws \Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
+   */
+  public function load(ContainerBuilder $container)
+  {
+    return $container->register('task.ecs', Ecs::class)
+      ->addArgument(new Reference('config'))
+      ->addArgument(new Reference('process_builder'))
+      ->addArgument(new Reference('formatter.raw_process'))
+      ->addTag('grumphp.task', ['config' => 'ecs']);
+  }
+}
